e59b45172799e4d1165f6d4ec08d47a2c0e46d59
DESTRUCTORH09 <kimkimk934@gmail.com>
2025-10-06 12:54:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,8 @@ const sections = document.querySelectorAll('.tab-section');
 
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
-    // Quitar active a todos los botones y secciones
     tabs.forEach(t => t.classList.remove('active'));
     sections.forEach(s => s.classList.remove('active'));
-
-    // Activar botón y sección correspondiente
     tab.classList.add('active');
     document.getElementById(tab.dataset.target).classList.add('active');
   });
